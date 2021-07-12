9bec4a80ed19e02c4a50505bdbb3d2c183a6ba46
Roman-Koval <kovalrv83@gmail.com>
2021-07-12 21:38:50 +0300

    исправил

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -1,21 +1,27 @@
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
- };
- 
- const themeEl = document.querySelector('#theme-switch-toggle');
- themeEl.addEventListener('change', onChangeToggle);
- localStorage.setItem('theme', Theme.LIGHT);
- 
- function onChangeToggle(e) {
-    const checked = e.currentTarget.checked;
-    if (checked) {
-       setDarkTheme();
-    } else { document.body.classList.remove(Theme.DARK); }
- }
- 
- function setDarkTheme() {
-     document.body.classList.remove(Theme.LIGHT);
-     document.body.classList.add(Theme.DARK);
-    localStorage.setItem('theme', Theme.DARK);
- }
+   LIGHT: 'light-theme',
+   DARK: 'dark-theme',
+};
+
+const themeEl = document.querySelector('#theme-switch-toggle');
+themeEl.addEventListener('change', onChangeToggle);
+localStorage.setItem('theme', Theme.LIGHT);
+
+function onChangeToggle(e) {
+   const checked = e.currentTarget.checked;
+   if (checked) {
+      setDarkTheme();
+   } else { document.body.classList.remove(Theme.DARK);
+            document.body.classList.add(Theme.LIGHT);
+             }
+}
+
+const theme = localStorage.getItem('theme') || Theme.LIGHT;
+document.body.classList.add(theme);
+themeEl.checked = theme === Theme.DARK;
+
+function setDarkTheme() {
+	document.body.classList.remove(Theme.LIGHT);
+	document.body.classList.add(Theme.DARK);
+   localStorage.setItem('theme', Theme.DARK);
+}
